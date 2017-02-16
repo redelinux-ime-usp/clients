3d4c4c256a09c1988ed508462ec91170da0cf1db
Carybé <carybe.silva@usp.br>
2017-02-16 14:57:50 -0200

    Updated the secrets

--- client.sh
+++ client.sh
@@ -17,7 +17,7 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 gpg -d secrets.tar.gz.gpg | tar -xz
 
 # To encrypt:
-# tar cz secrets | gpg -o secrets.tar.gz.gpg --cipher-algo AES256 --symmetric
+# tar cz secrets | gpg -o secrets.tar.gz.gpg --cipher-algo AES256 --digest-algo SHA512 --s2k-mode 3 --s2k-count 65011712 --compress-algo BZIP2 --bzip2-compress-level 9 --s2k-digest-algo SHA512 --symmetric
 
 # Global variables.
 FILES=$DIR/files
