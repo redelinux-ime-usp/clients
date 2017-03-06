0ed11e51dea36958b80bb2ac82e97773db9576b0
Carybé <carybe.silva@usp.br>
2017-03-06 09:07:50 -0300

    fixes the runner

--- run_client.sh
+++ run_client.sh
@@ -2,12 +2,17 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-chmod +x $DIR/client.sh
+pushd .
+
+cd $DIR
+
+chmod +x client.sh
 
 # Decrypt thy Secrets
-gpg -d $DIR/secrets.tar.gz.gpg | tar -xz
+gpg -d secrets.tar.gz.gpg | tar -xz
 
 # To encrypt:
-# tar cz secrets | gpg -o secrets.tar.gz.gpg --cipher-algo AES256 --digest-algo SHA512 --s2k-mode 3 --s2k-count 65011712 --compress-algo BZIP2 --bzip2-compress-level 9 --s2k-digest-algo SHA512 --symmetric
+#tar cz secrets | gpg -o secrets.tar.gz.gpg --cipher-algo AES256 --digest-algo SHA512 --s2k-mode 3 --s2k-count 65011712 --compress-algo BZIP2 --bzip2-compress-level 9 --s2k-digest-algo SHA512 --symmetric
 
-nohup client.sh &
+popd
+nohup $DIR/client.sh &
